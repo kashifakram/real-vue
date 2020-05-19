1355ced3aae574a7123cb39e7eccd176159d0cbb
Kashif Akram <k.akram@outlook.com>
2020-05-20 08:15:31 +1000

    initial commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Event Listing</h1>
+    <EventCard/>
+
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue';
+export default {
+    components: {
+        EventCard
+    },
+  data() {
+    return {
+      s: 5
+    };
+  }
+};
+</script>
---
+++ src/views/EventShow.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>Showing event #{{ Number(id) }}</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        id: {
+            default: 0
+        },
+    },
+}
+</script>
