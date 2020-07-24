f7797d9743be9290e7b89aa1c953970db6b65a78
Kashif Akram <k.akram@outlook.com>
2020-07-24 15:04:29 +1000

    gsap tweenin

--- src/views/Animations/Gsap.vue
+++ src/views/Animations/Gsap.vue
@@ -6,6 +6,9 @@
       <div id="container">
           <div v-for="(card) in cards" :key="card.id" class="icard"></div>
       </div>
+      <div class="bar" :style="{ width: tweenedNumber + 'px'}">
+          <span> {{ tweenedNumber.toFixed(0) }} </span>
+      </div>
   </div>
 </template>
 
@@ -22,10 +25,25 @@ export default {
         { id: 4 },
         { id: 6 },
         { id: 1 }
-      ]
+      ],
+      number: 0,
+      tweenedNumber: 0
     };
   },
+  watch: {
+    //   will watch value of number in data and run the following method when it changes
+    number(newValue) {
+      gsap.to(this.$data, {
+        duration: 1,
+        ease: 'circ.out',
+        tweenedNumber: newValue
+      });
+    }
+  },
   methods: {
+    randomNumber() {
+      this.number = Math.floor(Math.random() * (450 - 0));
+    },
     beforeEnter(el) {
       el.style.opacity = 0;
       el.style.transform = 'scale(0,0)';
@@ -49,16 +67,19 @@ export default {
       ease: 'power1',
       stagger: {
         each: 0.3,
-        from: 'edges'
+        from: 'end'
       }
     });
+  },
+  created() {
+    setInterval(this.randomNumber, 1500);
   }
 };
 </script>
 
 <style scoped>
 #container {
-  margin-top: 4em;
+  margin-top: 1em;
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
@@ -82,5 +103,15 @@ export default {
   border-radius: 1%;
   background-color: #16c0b0;
   box-shadow: 0.08em 0.03em 0.4em #ababab;
+}
+.bar {
+  margin-top: 1em;
+  padding: 5px;
+  background-color: #2c3e50;
+  border: 1px #16c0b0 solid;
+  min-width: 20px;
+}
+.bar span {
+  color: white;
 }
 </style>
